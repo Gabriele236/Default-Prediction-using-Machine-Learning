d56b41e5bf6bd0d0101a7bcd4c073b5ad85c7de3
Gabriele Amore <110041928+Gabriele236@users.noreply.github.com>
2023-09-01 17:58:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.metrics import classification_report
+import tensorflow as tf
+from sklearn.model_selection import KFold
 
 # LETTURA FILE CSV
 df = pd.read_csv("Dataset.csv", sep=";", header=0)
@@ -57,3 +59,36 @@ knn_cv_scores = cross_val_score(best_knn_model, X, y, cv=5)
 # Stampa delle prestazioni medie per K-NN
 print("K-NN Cross-Validation Scores:", knn_cv_scores)
 print("K-NN Mean Accuracy:", np.mean(knn_cv_scores))
+
+
+#Creazioni Rete Neurali
+nn_model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation = "relu", input_shape=(41,)),
+    tf.keras.layers.Dense(32, activation = "relu",),
+    tf.keras.layers.Dense(1, activation = "sigmoid")
+
+])
+
+nn_model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+
+
+num_folds = 5
+kf = KFold(n_splits = num_folds)
+
+fold_scores = []
+for train_index, test_index in kf.split(X):
+    X_train, X_val = X[train_index], X[test_index]
+    y_train, y_val = y[train_index], y[test_index]
+
+    nn_model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=0)
+    _, accuracy = nn_model.evaluate(X_val, y_val, verbose=0)
+    fold_scores.append(accuracy)
+
+mean_accuracy = np.mean(fold_scores)
+
+print(f"Mean CV Accurcy: {mean_accuracy}")
+
+
+
